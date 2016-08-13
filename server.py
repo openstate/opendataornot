28ec433acf23341f72628c75aeace5efa78273bb
Breyten Ernsting <bje@dds.nl>
2016-08-13 13:10:58 +0200

    Enable configuration from command line so we can run it on a different port.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import optparse
 
 from flask import (
     Flask, render_template, request, redirect, url_for, send_from_directory)
@@ -10,6 +11,51 @@ UPLOAD_FOLDER = '/Users/breyten/sammify/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 MAX_WIDTH = 200
 MAX_HEIGHT = 200
+
+def flaskrun(app, default_host="127.0.0.1",
+                  default_port="5000"):
+    """
+    Takes a flask.Flask instance and runs it. Parses
+    command-line flags to configure the app.
+    """
+
+    # Set up the command-line options
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help="Hostname of the Flask app " + \
+                           "[default %s]" % default_host,
+                      default=default_host)
+    parser.add_option("-P", "--port",
+                      help="Port for the Flask app " + \
+                           "[default %s]" % default_port,
+                      default=default_port)
+
+    # Two options useful for debugging purposes, but
+    # a bit dangerous so not exposed in the help message.
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+    parser.add_option("-p", "--profile",
+                      action="store_true", dest="profile",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    # If the user selects the profiling option, then we need
+    # to do a little extra setup
+    if options.profile:
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+
+        app.config['PROFILE'] = True
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
+                       restrictions=[30])
+        options.debug = True
+
+    app.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -64,4 +110,4 @@ def uploaded_file(filename):
                                filename)
 
 if __name__ == "__main__":
-    app.run()
+    flaskrun(app)
