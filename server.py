dbec1c14cefa710ac4a77f4bff9de983b5a072cd
Breyten Ernsting <bje@dds.nl>
2016-08-13 12:04:54 +0200

    Working proof of concept, without resizing an image to reasonable proportions.

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import os
 from flask import (
     Flask, render_template, request, redirect, url_for, send_from_directory)
 from werkzeug.utils import secure_filename
+from PIL import Image
 
 UPLOAD_FOLDER = '/Users/breyten/sammify/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -18,6 +19,11 @@ def main():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def process_image(image_file):
+    image = Image.open(image_file)
+    pixels = image.load()
+    return image, pixels
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -34,9 +40,15 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            image_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(image_file)
+            image, pixels = process_image(image_file)
+            return render_template(
+                'upload.html', image=image, pixels=pixels,
+                image_width=range(image.width),
+                image_height=range(image.height))
+            # return redirect(url_for('uploaded_file',
+            #                         filename=filename))
     return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
