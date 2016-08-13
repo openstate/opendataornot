4d938dc87d32f6f82c03c93c892ba8610c6d2eee
Breyten Ernsting <bje@dds.nl>
2016-08-13 14:05:16 +0200

    Basics of shortlink service.

--- server.py
+++ server.py
@@ -2,12 +2,16 @@
 import os
 import optparse
 import uuid
+from math import floor
+import string
 
 from flask import (
-    Flask, render_template, request, redirect, url_for, send_from_directory)
+    Flask, render_template, request, redirect, url_for, send_from_directory, g)
 from werkzeug.utils import secure_filename
 from PIL import Image
+import sqlite3
 
+DATABASE = '/Users/breyten/sammify/sammify.db'
 UPLOAD_FOLDER = '/Users/breyten/sammify/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 MAX_WIDTH = 200
@@ -63,6 +67,55 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_WIDTH'] = MAX_WIDTH
 app.config['MAX_HEIGHT'] = MAX_HEIGHT
 
+def toBase62(num, b = 62):
+    if b <= 0 or b > 62:
+        return 0
+    base = string.digits + string.lowercase + string.uppercase
+    r = num % b
+    res = base[r];
+    q = floor(num / b)
+    while q:
+        r = q % b
+        q = floor(q / b)
+        res = base[int(r)] + res
+    return res
+
+def toBase10(num, b = 62):
+    base = string.digits + string.lowercase + string.uppercase
+    limit = len(num)
+    res = 0
+    for i in xrange(limit):
+        res = b * res + base.find(num[i])
+    return res
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def table_check():
+    create_table = """
+        CREATE TABLE IMAGES (
+        ID INTEGER PRIMARY KEY     AUTOINCREMENT,
+        FILE  TEXT    NOT NULL
+        );
+        """
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute(create_table)
+            conn.commit()
+        except sqlite3.OperationalError as e:
+            print e
+            pass
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route("/")
 def main():
     return render_template('main.html')
@@ -72,11 +125,19 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 def process_image(image_file, max_width, max_height):
+    cursor = get_db().cursor()
+    insert_row = """
+        INSERT INTO IMAGES (FILE)
+            VALUES ('%s')
+        """ % (image_file,)
+    result_cursor = cursor.execute(insert_row)
+    get_db().commit()
+    encoded_string = toBase62(result_cursor.lastrowid)
     image = Image.open(image_file)
     # FIXME: this does not work reliably yet for some reason ...
     image.thumbnail((max_width, max_height), Image.ANTIALIAS)
     pixels = image.load()
-    return image, pixels
+    return encoded_string, image, pixels
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -98,14 +159,14 @@ def upload_file():
                 app.config['UPLOAD_FOLDER'],
                 u'%s.%s' % (str(uuid.uuid4()), file_ext,))
             file.save(image_file)
-            image, pixels = process_image(
+            encoded_string, image, pixels = process_image(
                 image_file, app.config['MAX_WIDTH'], app.config['MAX_HEIGHT'])
-            return render_template(
-                'upload.html', image=image, pixels=pixels,
-                image_width=range(image.width),
-                image_height=range(image.height))
-            # return redirect(url_for('uploaded_file',
-            #                         filename=filename))
+            # return render_template(
+            #     'upload.html', image=image, pixels=pixels,
+            #     image_width=range(image.width),
+            #     image_height=range(image.height))
+            return redirect(url_for('short_link',
+                                    encoded_string=encoded_string))
     return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
@@ -113,5 +174,21 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+@app.route('/i/<encoded_string>')
+def short_link(encoded_string):
+    image_id = toBase10(encoded_string)
+    cursor = get_db().cursor()
+    select_row = """
+            SELECT FILE FROM IMAGES
+                WHERE ID=%s
+            """ % (image_id)
+    result_cursor = cursor.execute(select_row)
+    try:
+        image_file = result_cursor.fetchone()[0]
+    except Exception as e:
+        image_file
+    return render_template('short_link.html', image_file=image_file)
+
 if __name__ == "__main__":
+    table_check()
     flaskrun(app)
