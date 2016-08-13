a272d8060949e9f5fb5190cd1f9532f61a9e4be7
Breyten Ernsting <bje@dds.nl>
2016-08-13 11:19:00 +0200

    Implement file uploading.

--- server.py
+++ server.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python
+import os
 
-from flask import Flask
-from flask import render_template
+from flask import (
+    Flask, render_template, request, redirect, url_for, send_from_directory)
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/Users/breyten/sammify/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def main():
     return render_template('main.html')
 
-@app.route("/upload")
-def upload():
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'picture' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['picture']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == "__main__":
     app.run()
