a79b2c72f933c94de4aee3733551186d0a6019e7
Breyten Ernsting <bje@dds.nl>
2016-08-13 10:27:55 +0200

    Templating setup.

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello World!"
+    return render_template('main.html')
 
 @app.route("/upload")
 def upload():
-    return "upload a file here!"
+    return render_template('upload.html')
 
 if __name__ == "__main__":
     app.run()
