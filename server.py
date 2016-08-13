c8d4a2d8939e5bdbeac16ab0473091adf0ef655d
Breyten Ernsting <bje@dds.nl>
2016-08-13 10:22:59 +0200

    Barebones flask setup.

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello World!"
+
+@app.route("/upload")
+def upload():
+    return "upload a file here!"
+
+if __name__ == "__main__":
+    app.run()
